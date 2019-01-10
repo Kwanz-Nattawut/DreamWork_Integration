efd2b91c8d3d052b7be7a5c9625fa29278626179
Kwanz-Nattawut <kwanznattawut@gmail.com>
2019-01-11 02:40:42 +0700

    three commit

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.get('/getSanam/:hour', (req, res) => {
             for(let i = 0 ; i < Object.keys(rsp).length ; i ++){
                 show.push(rsp[i].P_IN);
             }
-            Beacons.find({}).sort({"Timestamp" : -1}).limit(1).exec((err,test) => {
+            Beacons.find({}).sort({"Timestamp" : Date_Diff}).limit(1).exec((err,test) => {
                 console.log(test);
             });
             if(show.length == 0){
