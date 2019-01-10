dfdf55afca5c302cc96e1adf5c6ffa8c947f4767
Kwanz-Nattawut <kwanznattawut@gmail.com>
2019-01-10 17:41:11 +0700

    two commit

--- app.js
+++ app.js
@@ -99,9 +99,9 @@ csv
  .fromPath("sanam.csv")
  .on("data", function(data){
      let data_new = [];
-     data_new = data[0].split(";")
-     let data_0= data_new[6].split('');
-     console.log(data_0);
+     data_new = data[0][0]
+     //let data_0= data_new[6].split(' ');
+     console.log(data_new);
       //res.end(JSON.stringify(data));
       //res.json(JSON.stringify(data));
  })
