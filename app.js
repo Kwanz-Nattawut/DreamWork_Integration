12d229a4e1c93e17ba19bddd083f2c8f17b06fa2
Kwanz-Nattawut <kwanznattawut@gmail.com>
2019-01-10 23:19:41 +0700

    two commit

--- app.js
+++ app.js
@@ -59,9 +59,9 @@ app.get('/getSanam/:hour', (req, res) => {
             $gt :  Date_Diff
         }}).select('P_IN').exec((err,rsp) => {
             let show = [];
-            console.log(rsp[0].P_IN);
+            console.log(rsp[6].P_IN);
             for(let i = 0 ; i < Object.keys(show).length ; i ++){
-                show.push(rsp[i].P_IN);
+                //show.push(rsp[i].P_IN);
             }
              res.json(show);
              console.log(show,show.length);
