21b7a61d6a753b5c62a856d65f0d996f2d73ac0f
Kwanz-Nattawut <kwanznattawut@gmail.com>
2019-01-10 18:09:11 +0700

    two commit

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ csv
      let data_new = [];
      data_new = data[0];
      let data_0= data_new.split(';');
-     console.log(typeof(data));
+     console.log(data);
       //res.end(JSON.stringify(data));
       //res.json(JSON.stringify(data));
  })
