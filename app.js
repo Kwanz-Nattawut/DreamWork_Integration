c9950f25d0052dc78d35b4c16534865c7b0bd0c2
Kwanz-Nattawut <kwanznattawut@gmail.com>
2019-01-11 03:42:01 +0700

    three commit

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ app.get('/Temp_Hum/16', (req, res) => {
 });
 
 
-app.get('/test', (req, res) => {
+/*app.get('/test', (req, res) => {
 
     let data = [];
     let buff = [];
@@ -135,7 +135,7 @@ app.get('/test', (req, res) => {
             });
     });
 
-});
+});*/
 
 app.listen(3000, () => {
     console.log(`Server started on port 3000 \n http://localhost:3000/`);
