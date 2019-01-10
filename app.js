bcdaae3ffec12b6974dfaa60d09e80828dcd690a
Kwanz-Nattawut <kwanznattawut@gmail.com>
2019-01-10 11:50:28 +0700

    two commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var bodyParser = require('body-parser');
-
+var Sensors = require('./models/SensorData.model');
 var app = express();
 
 app.use(bodyParser.json());
@@ -11,6 +11,25 @@ app.get('/', (req, res) => {
     res.send("<h1>TESA TOP GUN REALLY 2019 DREAMWORK</h1>");
 });
 
+app.post('/HW_Send', (req, res) => {
+    var all = req.body;
+    var New = new Sensors(all);
+
+    New.save((err,rsp) => {
+        if(err){
+            console.log("error");
+        }
+        else
+        {
+             res.json(rsp);
+        }
+    })
+    console.log(all);
+});
+
+app.get('/Temp_Hum/16', (req, res) => {
+    
+});
 
 
 app.listen(3000, () => {
