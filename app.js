0d5f896bcc5a942701488d0ae1fb855b1b2967fb
wo-ko <bigname1101@gmail.com>
2019-01-11 02:49:51 +0700

    three commit

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.get('/getSanam/:hour', (req, res) => {
             for(let i = 0 ; i < Object.keys(rsp).length ; i ++){
                 show.push(rsp[i].P_IN);
             }
-            Beacons.find({ $min : Date_Diff_min}).limit(1).exec((err,test) => {
+            Beacons.find({ $min : "$P_IN"}).limit(1).exec((err,test) => {
                 console.log("test : ",test);
             });
             if(show.length == 0){
