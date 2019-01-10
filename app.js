4a3ecccca2e666f89b4c21785329eb72ca4e4990
Kwanz-Nattawut <kwanznattawut@gmail.com>
2019-01-10 22:51:05 +0700

    two commit

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ app.get('/test', (req, res) => {
                 console.log(data_arr[0].length,data_arr.length);
                 for(let j = 0 ; j < Object.keys(data_arr).length ; j++){
                     for(let i = 0 ; i < data_arr[0].length ; i ++){
-                        Beacons.insertMany({P_IN : data_arr[0][i + 1] , P_OUT : 0 , Timestamp : data_arr[0][0]});
+                        Beacons.insertMany({P_IN : data_arr[j][i + 1] , P_OUT : 0 , Timestamp : data_arr[j][0]});
                     }
                 } 
                 Beacons.find({}).exec((err,beacon) => {
