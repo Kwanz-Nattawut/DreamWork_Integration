c914eaf58ca6ce83b1aa0a1b4b5ec05d1f1c72b4
wo-ko <bigname1101@gmail.com>
2019-01-11 02:16:31 +0700

    three commit

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.get('/getSanam/:hour', (req, res) => {
             for(let i = 0 ; i < Object.keys(rsp).length ; i ++){
                 show.push(rsp[i].P_IN);
             }
-            
+            console.log(Beacons.find({}).min());
             if(show.length == 0){
                  res.json("ไม่พบข้อมูล");
             }
