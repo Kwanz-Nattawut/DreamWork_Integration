38136b2ff2de650c21748e77c550a0b518b1ad49
Kwanz-Nattawut <kwanznattawut@gmail.com>
2019-01-10 18:41:10 +0700

    two commit

--- app.js
+++ app.js
@@ -96,10 +96,10 @@ app.get('/test', (req, res) => {
             .on("end", function () {
                 resolve(data);
                 console.log(typeof(data[1]));
-                // for(let i=1; i<=data.length; i++){
-                //     buff[i] = data[i].split(';');
-                // }
-                // console.log(buff);
+                for(let i=1; i<=data.length; i++){
+                    buff[i] = data[i].toString().split(';');
+                }
+                console.log(buff);
             });
         //console.log(data[0])
     });
